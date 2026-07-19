ba1ffa36e05f93ea66b862c483de674f323f6169
Andrés Miguel Guzmán Cruz <guzmancruzandresmiguel@gmail.com>
2026-07-19 10:24:00 -0600

    Add: python-length-function - submission-0

---
+++ Python For Beginners/python-length-function/submission-0.py
@@ -0,0 +1,14 @@
+def get_longer_word(word1: str, word2: str) -> str:
+    a = len(word1)
+    b = len(word2)
+
+    if b < a:
+        return word1
+    else:
+        return word2
+
+
+# do not modify below this line
+print(get_longer_word("yellow", "orange"))
+print(get_longer_word("red", "blue"))
+print(get_longer_word("green", "blue"))
