a7aa918c3e64fe8fabc9b6b5704e94aae244f341
Andrés Miguel Guzmán Cruz <guzmancruzandresmiguel@gmail.com>
2026-08-02 13:36:15 -0600

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        for i in enumerate(s):
+            a.append(s[i])
+            b.append(t[i])
+
+        sorted(a)
+        sorted(b)   
+
+        print(a,b)
+
+        if a == b:
+            return True
+        else:
+            return False
